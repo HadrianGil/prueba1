0601eb5faefc8c33bdbe1808e46954f403b6525c
Nijirod <nicolasjimenezrdgz@gmail.com>
2024-02-08 22:23:53 +0100

    probando pyhton

---
+++ multi-nico.py
@@ -0,0 +1,11 @@
+numero = int(input("Introduce un numero: "))
+
+for i in range(10):
+    print(numero,"x",i,"=",numero*i)
+
+# for i in range(2,10,2):
+#    print (i)
+# # for(i=0;i<10;i++)
+    
+# print(range(0,10,2))
+    
